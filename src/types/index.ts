de9abdd139fa0c42202357c751ebbdd931a03a44
Akushu <tang.tang2004@gmail.com>
2025-06-19 17:21:50 +0700

    database

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+// Re-export all types for easy importing
+export * from './database';
+export * from './table_profiles';
+export * from './table_chat_sessions';
+export * from './table_chat_messages';
+export * from './table_quizzes';
+export * from './table_quiz_attempts';
+export * from './table_learning_progress';
+
+// Additional types
+export interface Question {
+  id: string;
+  question: string;
+  options: string[];
+  correct_answer: number | number[];
+  type?: 'single' | 'multiple';
+  explanation: string;
+}
