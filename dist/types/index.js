a0203b25ea00793c5a0190f00eaec380f85bd415
Akushu2357 <tang.tang2004@gmail.com>
2025-06-23 15:13:17 +0700

    Deploy application to Netlify

--- dist/types/index.js
+++
@@ -1,9 +0,0 @@
-// Re-export all types for easy importing
-export * from './database';
-export * from './table_profiles';
-export * from './table_chat_sessions';
-export * from './table_chat_messages';
-export * from './table_quizzes';
-export * from './table_quiz_attempts';
-export * from './table_learning_progress';
-export * from './table_schedule';
