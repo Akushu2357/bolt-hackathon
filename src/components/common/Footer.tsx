9c6d73234de0a2b7aba8d16b0f3a3b10eabd3539
Atom1l <atom7548@gmail.com>
2025-06-26 14:03:31 +0700

    Updated Footer.tsx

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-white border-t border-gray-200 py-8 mt-16">
+    <footer className="bg-white border-t border-gray-200 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center justify-center space-y-4">
           {/* Bolt Logo Link */}
